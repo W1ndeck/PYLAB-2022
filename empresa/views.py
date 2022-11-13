2a348d321b51964922a7a16f5e10d227a4efa50b
Lucas Mateus <lmateus1067@outlook.com>
2022-11-13 17:02:06 -0300

    add template

--- empresa/views.py
+++ empresa/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 def nova_empresa(request): #funcoa a ser retornada no urls
 
-    return HttpResponse('Estou em nova_empresa')
+    return render(request, 'nova_empresa.html')
